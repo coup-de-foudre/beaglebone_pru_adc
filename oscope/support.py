30d93f3c5dee14eb70372482e35da10b8e784ac6
meawoppl <meawoppl@gmail.com>
2019-01-04 07:57:36 -0800

    Tabnanny got me

--- oscope/support.py
+++ oscope/support.py
@@ -6,7 +6,7 @@ def ndarray_to_bytes(array: np.ndarray) -> bytes:
     Encode a np array to bytes in .npy format
     """
     bio = io.BytesIO()
-	np.save(bio, array, allow_pickle=False)
+    np.save(bio, array, allow_pickle=False)
     return bio.getvalue()
 
 def bytes_to_ndarray(b: bytes) -> np.ndarray:
