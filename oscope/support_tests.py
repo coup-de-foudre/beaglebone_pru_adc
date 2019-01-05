74b231e5073c5bff7a976699ba4ba47c54cd0349
meawoppl <meawoppl@gmail.com>
2019-01-04 20:39:36 -0800

    Test improvements

--- oscope/support_tests.py
+++ oscope/support_tests.py
@@ -16,7 +16,7 @@ def test_to_from_bytes():
 def test_idempotence():
     a1 = np.arange(107)
     b1 = support.ndarray_to_bytes(a1)
-    time.sleep(1)
+    time.sleep(0.1)
     b2 = support.ndarray_to_bytes(a1)
 
-    assert b1 == b2
+    assert b1 == b2, "Idempotence check failed :("
