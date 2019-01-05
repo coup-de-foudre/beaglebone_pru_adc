7cc0bfee4a522160e37c1df163e33eddee7e13f6
meawoppl <meawoppl@gmail.com>
2019-01-04 21:22:12 -0800

    IPC test helpers

--- oscope/zmq_socket_helpers.py
+++ oscope/zmq_socket_helpers.py
@@ -1,4 +1,51 @@
+import tempfile
+import os
+
 import zmq
+
+IPC_PATH_MAX_LEN = 107
+
+
+class IPCTemp:
+    """
+    This class provides unique and sanitary IPC addresses
+    on the local filesystem.  It is designed to be used
+    as a context manager in the following pattern:
+
+    >>> with IPCTemp(["foo", "bar"]) as (foo_path, bar_path):
+    >>>    socket1.bind(foo_path)
+    >>>    socket2.bind(bar_path)
+
+    On exit of the block, the context manager will ensure deletion
+    of the root folder as well as any sockets found within it.
+    Each path from a particular invocation has a unique
+    directory prefix, so this context manager can be used in a
+    nested fashion without fear of name collision.
+    """
+    def __init__(self, ipc_names: list):
+        self._ipc_names = ipc_names
+        assert len(ipc_names) == len(set(ipc_names)), \
+            "All ipc names must be unique:" + repr(ipc_names)
+
+    def __enter__(self):
+        self._td = tempfile.TemporaryDirectory(prefix="ipc_temp")
+        self._td.__enter__()
+        names = ["ipc://" + os.path.join(self._td.name, ipc_name) for ipc_name in self._ipc_names]
+
+        for name in names:
+            if len(name) >= IPC_PATH_MAX_LEN:
+                self._td.cleanup()
+                kesm.base.raise_error(
+                    'IPC Address name "{}" is too long: ({} chars, max is {})',
+                    name, len(name), IPC_PATH_MAX_LEN,
+                    exception_class=AssertionError
+                )
+        return names
+
+    def __exit__(self, *args):
+        self._td.__exit__(*args)
+
+
 
 class PublishContext:
     """
@@ -6,14 +53,18 @@ class PublishContext:
     ZMQ Publish socket instances.
 
     >>> with socket as PublishContext(["ipc://endpoint"]):
-    ...    # Do stuff with socket
-    ... # Socket and context cleaned up w/o hassle
+    >>>    # Do stuff with socket
+    >>> # Socket and context cleaned up w/o hassle
     """
 
-    def __init__(self, binds: list):
-        for element in binds:
-            assert isinstance(element, str)
+    @staticmethod
+    def sanity_check_bind_list(binds: list):
+        for bind in binds:
+            assert isinstance(bind, str)
+            assert ":" in bind, "Invalid endpoint specified:" + bind
 
+    def __init__(self, binds: list):
+        self.sanity_check_bind_list(binds)
         self._binds = binds
         self._socket = None
         self._ctx = None
@@ -34,3 +85,8 @@ class PublishContext:
         if self._ctx is not None:
             self._ctx.term()
             self._ctx = None
+
+class TestPublishContext(PublishContext):
+    def __init__(self, binds):
+        self.sanity_check_bind_list(binds)
+    
--- oscope/zmq_socket_helpers_tests.py
+++ oscope/zmq_socket_helpers_tests.py
@@ -2,6 +2,17 @@ import zmq
 
 import oscope.zmq_socket_helpers as socket_helpers
 
+
 def test_ctx_manager():
     with socket_helpers.PublishContext(("ipc://foo", "ipc://bar")) as pub_socket:
         assert isinstance(pub_socket, zmq.Socket)
+
+
+def test_ipc_temp_unique():
+    with socket_helpers.IPCTemp(["test1"]) as paths1:
+        with socket_helpers.IPCTemp(["test1"]) as paths2:
+            unique_names = set()
+            unique_names.update(paths1)
+            unique_names.update(paths2)
+            
+            assert len(unique_names) == 2, unique_names
